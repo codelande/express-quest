d733aabec9338aa92b9a8cee21c88d8886d18c39
codelande <corentin.delande.1@gmail.com>
2022-10-17 10:16:12 +0200

    pur toutes

--- movieHandlers.js
+++ movieHandlers.js
@@ -39,10 +39,34 @@ const postMovie = (req, res) => {
       [title, director, year, color, duration]
     )
     .then(([result]) => {
-      res.location(`/api/movies/${result.insertId}`).sendStatus(201);    })
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
     .catch((err) => {
       console.error(err);
       res.status(500).send("Error saving the movie");
+    });
+};
+
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
     });
 };
 
@@ -50,4 +74,5 @@ module.exports = {
   getMovies,
   getMovieById,
   postMovie,
+  updateMovie,
 };
