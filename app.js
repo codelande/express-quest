371a764f651eb0de5dc984cb7b742f083754d7f4
codelande <corentin.delande.1@gmail.com>
2022-10-18 10:02:35 +0200

    test

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
+// test github
 
 app.listen(port, (err) => {
   if (err) {
