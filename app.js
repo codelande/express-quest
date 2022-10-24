b5ee9bdf902b50a9fe9442c233012c5896ecd0ea
codelande <corentin.delande.1@gmail.com>
2022-10-24 12:18:54 +0200

    add verify token

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const { validateMovie, validateUser } = require("./validators.js");
-const { hashPassword } = require("./auth.js");
+const { hashPassword, isItDwight, verifyPassword, verifyToken } = require("./auth.js");
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
 const app = express();
 app.use(express.json());
@@ -12,18 +14,22 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+// the public routes
+
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/login", userHandlers.getUserByEmail, verifyPassword)
 app.post("/api/users", hashPassword, validateUser, userHandlers.postUser);
+
+// then the routes to protect
+
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+
+app.post("/api/movies", verifyToken, validateMovie, movieHandlers.postMovie);
 
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
